c45392bb380e95ec106ee5531672ee8d4d545463
HuyenTran75 <118829380+HuyenTran75@users.noreply.github.com>
2022-12-07 00:30:35 +0700

    Add files via upload

---
+++ LE HUYEN TRAN_LAB4.sql
@@ -0,0 +1,107 @@
+﻿--Viết chương trình xem xét có tăng lương cho nhân viên hay không. 
+--Hiển thị cột thứ 1 là TenNV, cột thứ 2 nhận giá trị
+
+select iif(luong>=ltb,'khong tang luong','tang luong') as thuong,tennv,luong,ltb
+from
+(select tennv,luong,ltb from NHANVIEN,
+(select phg,avg(luong) as 'ltb' from NHANVIEN group by phg) as temp where NHANVIEN.PHG=temp.PHG) as abc
+
+select * from NHANVIEN
+select phg,avg(luong) as 'ltb' from NHANVIEN group by phg
+
+--Viết chương trình phân loại nhân viên dựa vào mức lương.
+declare @thongke table (phg int, LTB float);
+insert into @thongke
+select PHG,avg(luong) as LTB from NHANVIEN group by PHG;
+select TENNV, LUONG,'chuc vu' = iif(luong>b.LTB,'truong phong','thanh vien')
+from NHANVIEN a inner join @thongke b on a.PHG=b.phg;
+
+
+--.Viết chương trình hiển thị TenNV như hình bên dưới, tùy vào cột phái của nhân viên
+
+select tenv = case Phai
+when 'nam' then 'Mr.'+[TenNV]
+else
+  'MS.' +[TenNV]
+end
+from NHANVIEN
+
+--Viết chương trình tính thuế mà nhân viên phải đóng theo công thức:
+select tennv,luong,thue=case 
+when luong between 0 and 25000 then luong*0.1
+when luong between 25000 and 30000 then luong*0.12
+when luong between 30000 and 40000 then luong*0.15
+when luong between 40000 and 50000 then luong*0.2
+ else
+ luong*0.25
+end
+ 
+from NHANVIEN
+ --Cho biết thông tin nhân viên (HONV, TENLOT, TENNV) có MaNV là số chẵn.
+ select * from NHANVIEN
+ declare @dem int=2;
+ while @dem<(select count(manv) from NHANVIEN)
+ begin
+ select* from NHANVIEN where cast (manv as int)= @dem
+ set @dem= @dem +2;
+ end
+ --Cho biết thông tin nhân viên (HONV, TENLOT, TENNV) có MaNV là số chẵn nhưng
+--không tính nhân viên có MaNV là 4.
+select * from NHANVIEN;
+
+declare @max int, @num int;
+
+select @max = max(cast(manv as int)) from NHANVIEN;
+
+set @num = 1;
+
+while @num<=@max
+begin 
+if  @num =4
+begin
+set @num = @num +1;
+continue;
+end
+if @num % 2 =0
+select MANV,HONV,TENLOT,TENNV from NHANVIEN where cast(manv as int) = @num;
+set @num = @num +1;
+end;
+--Thực hiện chèn thêm một dòng dữ liệu vào bảng PhongBan theo 2 bước
+select * from PHONGBAN;
+
+ begin try
+ insert into PHONGBAN(TENPHG,MAPHG,TRPHG,NG_NHANCHUC)
+ values (N'sản xuất',9,'009','2020-03-02');
+	print N'them du lieu thanh cong';
+ end try
+begin catch
+ print N'failure: chen du lieu that bai';
+ end catch;
+ --Chèn sai kiểu dữ liệu cột MaPHG để nhận thông báo lỗi “Them dư lieu that bai”
+--từ khối Catch
+select * from PHONGBAN;
+
+ begin try
+ insert into PHONGBAN(TENPHG,MAPHG,TRPHG,NG_NHANCHUC)
+ values (N'sản xuất',7,'009','2020-03-02');
+	print N'them du lieu thanh cong';
+ end try
+begin catch
+ print N'failure: chen du lieu that bai';
+ end catch;
+
+ --Viết chương trình khai báo biến @chia, thực hiện phép chia @chia cho số 0 và dùng
+--RAISERROR để thông báo lỗi.
+begin try
+declare @chia int;
+
+set @chia =55/0;
+end try
+begin catch
+declare @errorMessage nvarchar(2048), @errorSeverity int, @errorState int;
+select @errorMessage = error_message(),
+		@errorSeverity = error_severity(),
+		@errorState = error_state();
+
+	raiserror(@errorMessage, @errorSeverity, @errorState);
+end catch;
---
+++ LE HUYEN TRAN_LAB5.sql
@@ -0,0 +1,159 @@
+﻿---- BAI1 ----
+--a--
+create procedure lab5_B1_a @name nvarchar(20)
+as 
+	begin
+		print 'Xin chao: '+ @name
+	end
+execute lab5_B1_a N'Huyền Trân'
+--b--
+create procedure lab5_B1_b @s1 int, @s2 int
+as 
+	begin
+		declare @sum int=0
+		set @sum = @s1 + @s2
+		print 'Tong: ' + cast(@sum as varchar(10))
+	end
+execute lab5_B1_b 3,4
+--c--
+create procedure lab5_B1_c @n int
+as
+	begin
+		declare @tg int = 0, @i int = 0;
+		while @i < @n
+			begin 
+				set @tg = @tg + @i 
+				set @i = @i + 2 
+			end
+		print N'Tổng số chẵn là: ' + cast(@tg as varchar(10))
+		end
+execute lab5_B1_c 8
+--d--
+create procedure lab5_B1_d @a int, @b int
+as
+	begin
+		while (@a != @b)
+			begin 
+				if(@a > @b)
+					set @a = @a - @b
+				else
+					set @b = @b - @a
+			end
+			return @a
+	end
+declare @c int
+execute @c = lab5_B1_d 160,40
+print @c
+
+---- BAI2 ----
+--a--a-
+create procedure lab5_B2_a @Manv nvarchar(20)
+as 
+	begin
+		select * from NHANVIEN where MaNV = @Manv
+	end
+execute lab5_B2_a '005'
+--b--b--
+create procedure lab5_B2_B @MaDa numeric(20)
+as 
+	begin
+		select count(MaNV) 
+		as 'so luong nhan vien tham gia de an', MADA, TENPHG
+		from NHANVIEN inner join PHONGBAN 
+		on NHANVIEN.PHG= PHONGBAN.MAPHG
+		inner join  DEAN on DEAN.PHONG =PHONGBAN.MAPHG
+		where MADA = @MaDa
+		group by TENPHG, MADA
+	end
+execute lab5_B2_B '30'
+--c--c--
+create procedure lab5_B2_C 
+	@MaDa numeric(20), @Ddiem_DA nvarchar(15)
+as 
+begin
+	select count(b.ma_nvien) as 'so luong'
+	from DEAN a inner join PHANCONG b on a.MADA=b.MADA
+	where a.MADA=@MaDa and a.Ddiem_DA = @DDiem_DA;
+end;
+
+execute lab5_B2_C 3,N'TP HCM'
+---- BAI3 ----
+----3.1----
+create procedure lab5_B3_A 
+	@MaPB int, @TenPB nvarchar(15),
+	@MaTP nvarchar(9), @NgayNhanChuc date
+as
+	begin
+		if(exists(select * from PhongBan where MaPHG = @MaPB ))
+			print 'Them That Bai'
+		else
+			begin
+				insert into PHONGBAN(MAPHG, TENPHG, TRPHG, NG_NHANCHUC)
+				values(@MaPB,@TenPB,@MaTP,@NgayNhanChuc)
+				print 'Them Thanh Cong'
+			end
+	end
+
+execute lab5_B3_A  '8','CNTT','008','2020-10-06'
+select * from PHONGBAN;
+
+----3.2---
+create procedure lab5_B3_B 
+	@TenPHG_cu nvarchar(15),
+	@TENPHG nvarchar(15),
+	@MAPHG int,
+	@TRPHG nvarchar(9),
+	@NG_NHANCHUC date
+as
+begin
+	UPDATE [dbo].[PHONGBAN]
+		SET [TENPHG] = @TENPHG
+			,[MAPHG] = @MAPHG
+			,[TRPHG] = @TRPHG
+			,[NG_NHANCHUC] = @NG_NHANCHUC
+		WHERE TENPHG= @TenPHG_cu;
+end;
+
+execute lab5_B3_B  'CNTT','IT',10, '005', '1-1-2020'
+SELECT * FROM PHONGBAN;
+
+----3.3----
+select HONV, TENLOT,TENNV, MANV,NGSINH,DCHI,PHAI,LUONG,MA_NQL,PHG from NHANVIEN 
+insert into NHANVIEN(HONV, TENLOT, TENNV, MANV,NGSINH,DCHI,PHAI, LUONG,MA_NQL, PHG) 
+values ('Nguyen', 'Van', 'Nam', '018', '2020-06-10', 'Da Nang', 'Nam', '25000', '004', '4') 
+
+create procedure spInsertNhanVien
+@HONV nvarchar(15), @TENLOT nvarchar(15),@TENNV nvarchar(15),
+@MANV nvarchar(6),@NGSINH date,@DCHI nvarchar(50),@PHAI nvarchar(3), 
+@LUONG float,@MA_NQL nvarchar(3) = null , @PHG int 
+as
+begin --proc 
+	declare @age int
+	set @age = YEAR(GETDATE()) - YEAR(@NGSINH)
+	if @PHG =(select MaPHG from PHONGBAN where TENPHG = 'IT')
+		begin --thuoc phong IT
+			if @LUONG < 25000
+				set @MA_NQL = '009'
+			else set  @MA_NQL = '005'
+			if(@PHAI = 'Nam' and (@age >=18 and @age <=65))
+				or (@PHAI = N'Nu' and (@age >=18 and @age <=60))
+				begin --do tuoi lao dong
+
+					insert into NHANVIEN(HONV, TENLOT, TENNV, MANV,NGSINH,DCHI,
+										PHAI, LUONG,MA_NQL, PHG) 
+					values (@HONV, @TENLOT, @TENNV, @MANV, @NGSINH, @DCHI,
+										@PHAI, @LUONG, @MA_NQL, @PHG) 
+
+				end --do tuoi lao dong
+			else
+				print 'khong thuoc do tuoi lao dong'
+		end --thuoc phong IT
+	else
+		print 'Khong phai phong ban IT'
+end
+execute spInsertNhanVien 'Nguyen', 'Van', 'Nam', '018', '2020-06-10', 'Da Nang',
+							'Nam', '25000', '004', '4'
+execute spInsertNhanVien 'Nguyen', 'Van', 'Nam', '019', '1955-06-10', 'Da Nang',
+							'Nam', '25000', '004', '4'
+execute spInsertNhanVien 'Nguyen', 'Hoang', 'Anh', '020', '1960-06-10', 'Da Nang',
+							'Nam', '25000', '004', '4'
