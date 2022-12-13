32a4a345a3c3acb260f0a788a738b1cd8bb92652
buiblue <118748695+buiblue@users.noreply.github.com>
2022-12-13 13:52:30 +0700

    Add files via upload

---
+++ KT.sql
@@ -0,0 +1,42 @@
+﻿-- tạo bảng
+CREATE TABLE LOP
+(
+	MALOP CHAR(3),
+	TENLOP VARCHAR(40),
+	SISO TINYINT,
+	CONSTRAINT PK_LOP PRIMARY KEY (MALOP)
+)
+CREATE TABLE SINHVIEN
+(
+	MASV CHAR(5),
+	HOTEN VARCHAR(40),
+	NGAYSINH SMALLDATETIME,
+	MALOP CHAR(3),
+	CONSTRAINT PK_SINHVIEN PRIMARY KEY (MASV)
+)
+CREATE TABLE MONHOC
+(
+	MAMH VARCHAR(10),
+	TENMH VARCHAR(40),
+	CONSTRAINT PK_MONHOC PRIMARY KEY (MAMH)
+)
+CREATE TABLE KETQUA
+(
+	MASV CHAR(5),
+	MAMH VARCHAR(10),
+	DIEMTHI NUMERIC(4,2)
+	CONSTRAINT PK_KETQUATHI PRIMARY KEY (MASV, MAMH)
+)
+
+--câu 1:
+create function F_DiemTB(@MaSV varchar(10))
+returns float
+as
+begin
+declare @kq float
+select @kq=avg(tb1.Diem)from 
+(select distinct KetQua.MaSV, dbo.F_DiemThi(@masv,KetQua.MaMH) as Diem from KetQua 
+where KetQua.MaSV=@masv)as tb1
+return @kq
+end
+--CÂU 2:
